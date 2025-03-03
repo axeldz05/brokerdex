801b33c9289cda158b67a9d152863a93a78025ff
axeldz05 <axeldz0512@gmail.com>
2025-03-03 19:11:42 -0300

    separate form of User and Account

--- account/views.py
+++ account/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login
-from .forms import RegistrationForm
+from .forms import AccountForm, CustomUserForm
 
 def log_in(request):
     if request.method == "POST":
@@ -15,10 +15,16 @@ def log_in(request):
 
 def register(request):
     if request.method == "POST":
-        form = RegistrationForm(request.POST)
-        if form.is_valid():
-            login(request, form.save())
+        user_form = CustomUserForm(request.POST)
+        acc_form = AccountForm(request.POST)
+        if acc_form.is_valid() and user_form.is_valid():
+            user = user_form.save()
+            acc = acc_form.save(commit=False)
+            acc.user = user
+            acc.save()
+            login(request, user)
             return redirect("index")
     else:
-        form = RegistrationForm()
-    return render(request,'account/register.html',{"form": form})
+        user_form = CustomUserForm()
+        acc_form = AccountForm()
+    return render(request,'account/register.html',{"acc_form": acc_form, "user_form": user_form})
